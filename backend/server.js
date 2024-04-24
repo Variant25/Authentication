eab39bcfabbb5ccf84395ec9ca50bfe662605a05
Ayush <ayushp041@gmail.com>
2024-04-25 01:56:07 +0530

    day 1

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,24 @@
+require('dotenv').config()
+
 const express =require('express');
+
+//express app
 const app=express();
 
+//middleware
+app.use((req,res,next)=>{
+    console.log(req.path,req.method);
+    next();
+})
 
-app.listen(4000,()=>{
-    console.log('listening on port 4000')
+
+//routes
+app.get("/",(req,res)=>{
+    res.json({mssg: "Welcome to our APP"});
+})
+
+
+//listen 4 req
+app.listen(process.env.PORT,()=>{
+    console.log('listening on port', process.env.PORT);
 })
