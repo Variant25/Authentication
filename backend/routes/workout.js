194e5190cc27fdf8f9d70c6fb8dc96af2ebc977a
Variant25 <ayushp041@gmail.com>
2024-07-04 21:09:22 +0530

    controller created

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,29 +1,21 @@
 const express = require('express')
 const router = express.Router()
-
+const mongoose = require('mongoose');
+const Workout = require('../models/workoutModel')
+const {createWorkout,getWorkouts,getWorkout,deleteWorkout,updateWorkout} = require('../controller/workoutController')
 //GET all workouts
-router.get('/',(req,res)=>{
-    res.json({mssg:'get all workout'})
-})
+router.get('/',getWorkouts)
 
 //get a single workout
-router.get('/:id',(req,res)=>{
-    res.json({mssg:'get a single workout'})
-})
+router.get('/:id',getWorkout)
 
 //post a new workout
-router.post('/',(req,res)=>{
-    res.json({mssg:'post a new workout'})
-})
+router.post('/',createWorkout)
 
 //delete a workout
-router.delete('/:id',(req,res)=>{
-    res.json({mssg:'delete a workout'})
-})
+router.delete('/:id',deleteWorkout)
 
 //update a workout
-router.patch('/:id',(req,res)=>{
-    res.json({mssg:'update a workout'})
-})
+router.patch('/:id',updateWorkout)
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express =require('express')
 const workoutRoutes = require('./routes/workout')
-
+const mongoose = require('mongoose')
 
 //express app 
 const app = express()
 
+
+app.use(express.json());
 //middleware
 app.use((req,res,next)=>{
     console.log(req.path,req.method)
@@ -15,8 +17,15 @@ app.use((req,res,next)=>{
 //routes
 app.use('/api/workouts', workoutRoutes)
 
-//listen for req
-
-app.listen(process.env.PORT,()=>{
-    console.log('listening on port 4000n')
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    //listen for req
+    app.listen(process.env.PORT,()=>{
+    console.log('Connected to DB & listening on port 4000')
+    })
 })
+.catch((error)=>{
+    console.log(error)
+})
+
